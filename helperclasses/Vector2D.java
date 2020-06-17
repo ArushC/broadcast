47e6a2cd8bf2493248e820380d52534abc79b73f
ArushC <65872167+ArushC@users.noreply.github.com>
2020-06-17 16:02:39 -0700

    Create Vector2D.java

---
+++ helperclasses/Vector2D.java
@@ -0,0 +1,69 @@
+package helperclasses;
+import com.herumi.mcl.*;
+
+public class Vector2D {
+
+	private Fr x;
+	private Fr y;
+	
+	public Vector2D() {
+		//default is (1, 1)
+		this.x = new Fr(1);
+		this.y = new Fr(1);
+	}
+	
+	public Vector2D(Fr x, Fr y) {	
+		
+		this.x = new Fr(x);
+		this.y = new Fr(y);
+		
+	}
+	
+	
+	//randomly set the x & y coordinates of this vector
+	public void setByCSPRNG() {
+		
+		Fr x = new Fr();
+		x.setByCSPRNG();
+		Fr y = new Fr();
+		y.setByCSPRNG();
+		this.x = new Fr(x);
+		this.y = new Fr(y);
+		
+	}
+	
+	//scalar multiplication
+	public Vector2D multiply(Fr scalar) {
+		
+		Fr newX = new Fr();
+		Fr newY = new Fr();
+		Mcl.mul(newX, scalar, this.x);
+		Mcl.mul(newY, scalar, this.y);
+		return new Vector2D(newX, newY);
+		
+	}
+	
+	public Fr dotProduct(Vector2D v) {
+		
+		Fr result = new Fr();
+		Fr vx = v.getX();
+		Fr vy = v.getY();
+		Fr xProduct = new Fr();
+		Fr yProduct = new Fr();
+		Mcl.mul(xProduct, this.x, vx);
+		Mcl.mul(yProduct, this.y, vy);
+		Mcl.add(result, xProduct, yProduct);
+		return result;
+		
+	}
+	
+	//getter methods
+	public Fr getX() {
+		return this.x;
+	}
+	
+	public Fr getY() {
+		return this.y;
+	}
+	
+}
