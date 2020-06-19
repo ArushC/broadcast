0e637afa02b319fb193a46f6be2a617755f5aafe
ArushC <65872167+ArushC@users.noreply.github.com>
2020-06-19 12:33:51 -0700

    Update IBBESystem.java

--- schemes/IBBESystem.java
+++ schemes/IBBESystem.java
@@ -9,6 +9,7 @@ import java.io.*;
 import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
 
 //The scheme: https://eprint.iacr.org/2008/268.pdf (4.1, page 10)
 public class IBBESystem {
@@ -17,11 +18,14 @@ public class IBBESystem {
 	private static Fr alpha, gamma, t;
 	private static G2 g2;
 	private static GT K;
+	private static int lambda;
 	//PRECONDITION: G is of order p >= n + l, l <= n
 	//input: n = # of users, l = maximal size of a broadcast recipient group
 	//output: Object[] containing the public key PK and private key SK
-	public static Object[] setup(int n, int l) {
+	public static Object[] setup(int n, int l, int lambda) {
 		
+		Mcl.SystemInit(lambda);
+		IBBESystem.lambda = lambda;
 		//instead of creating a GroupGen function, the groups are generated here
 		
 		//generate random g1, g2
@@ -120,7 +124,7 @@ public class IBBESystem {
 		
 		//1. compute ri = phi(kappa, i) where phi is a PRF
 		phi = new CustomPRF(kappa, i);
-		Fr ri = phi.compute();
+		Fr ri = phi.compute(lambda);
 		
 		//2. compute hi = g2^((gamma - ri)/(alpha - i))
 		G2 hi = new G2();
@@ -344,15 +348,118 @@ public class IBBESystem {
 		
 	}
 	
+	private static long[] printRuntimes(int n, int l, int lambda) {
+		
+		long startSetup = System.nanoTime();
+		Object[] setup = setup(n, l, lambda);
+		long elapsedSetup = System.nanoTime() - startSetup;
+		double secondsSetup = ((double) elapsedSetup) / 1E9;
+		
+		ArrayList<Object> PK = (ArrayList<Object>) setup[0];
+		Fr[] MSK = (Fr[]) setup[1];
+		
+		ArrayList<Integer> S = new ArrayList<Integer>();
+		//randomly generate numbers to put in the subset S (NO REPEATS)
+		ArrayList<Integer> randomNums = new ArrayList<Integer>();
+		for (int i = 0; i < n; i++) { 
+			randomNums.add(i + 1);
+		}
+		for (int i = 0; i < l; i++) {
+			int randomIndex = ThreadLocalRandom.current().nextInt(1, randomNums.size());
+			int randomID = randomNums.get(randomIndex);
+			S.add(randomID);
+			randomNums.remove(randomIndex);
+		}
+		
+		//generate ciphertext and keys
+		int i = S.get(ThreadLocalRandom.current().nextInt(0, S.size()));
+		long startEncrypt = System.nanoTime();
+		Object[] C = enc(S, PK);
+		long elapsedEncrypt = System.nanoTime() - startEncrypt;
+		double secondsEncrypt = ((double) elapsedEncrypt) / 1E9;
+		long startKeyGen = System.nanoTime();
+		Object[] di = keyGen(i, MSK);
+		long elapsedKeyGen = System.nanoTime() - startKeyGen;
+		double secondsKeyGen = ((double) elapsedKeyGen) / 1E9;
+		
+		//finally, decrypt
+		Fr[] tau = (Fr[]) C[0];
+		Object[] Hdr = (Object[]) C[1];
+		GT K1 = (GT) C[2];
+		long startDecrypt = System.nanoTime();
+		GT K2 = decrypt(S, i, di, tau, Hdr, PK);
+		long elapsedDecrypt = System.nanoTime() - startDecrypt;
+		double secondsDecrypt = ((double) elapsedDecrypt) / 1E9;
+		
+		String result = (K1.equals(K2)) ? "SUCCESSFUL DECRYPTION" : "FAILED DECRYPTION";
+		System.out.println(result + ": n = " + n + ", l = " + l);
+		System.out.println(); //padding
+		System.out.println("setup took " + secondsSetup + " seconds");
+		System.out.println("encryption took " + secondsEncrypt + " seconds");
+		System.out.println("key generation took " + secondsKeyGen + " seconds");
+		System.out.println("decryption took " + secondsDecrypt + " seconds");
+		System.out.println(); //more padding
+		
+		long[] elapsedTimes = new long[4];
+		elapsedTimes[0] = elapsedSetup;
+		elapsedTimes[1] = elapsedEncrypt;
+		elapsedTimes[2] = elapsedKeyGen;
+		elapsedTimes[3] = elapsedDecrypt;
+		
+		return elapsedTimes;
+		
+	}
+	
+
+public static double[] testRuntimes(int lambda) {
+		
+		long totalSetupTime = 0, totalEncryptionTime = 0, totalKeyGenTime = 0, totalDecryptionTime = 0;
+		
+		int count = 0;
+		for (int n = 5000; n < 100000; n+= 5000) {
+			long[] elapsedTimes = printRuntimes(n, 100, lambda); //constant l = 100; changing n does not affect the other times
+			count++;
+			totalSetupTime += elapsedTimes[0];
+			totalEncryptionTime += elapsedTimes[1];
+			totalKeyGenTime += elapsedTimes[2];
+			totalDecryptionTime += elapsedTimes[3];
+		}
+		
+		count = 0;
+		for (int l = 10; l <= 300; l += 10) {
+			printRuntimes(100000, l, lambda); //value of n really doesn't matter, as long as it's constant
+		}
+		double averageSetupTime = ((double) totalSetupTime) / (1E9 * count);
+		double averageEncryptionTime = ((double) totalEncryptionTime) / (1E9 * count);
+		double averageKeyGenTime = ((double) totalKeyGenTime) / (1E9 * count);
+		double averageDecryptionTime = ((double) totalDecryptionTime) / (1E9 * count);
+		
+		System.out.println("Average setup time, constant l = 100: " + averageSetupTime + " seconds");
+		System.out.println("Average encryption time, constant l = 100: " + averageEncryptionTime + " seconds");
+		System.out.println("Average key generation time, constant l = 100: " + averageKeyGenTime + " seconds");
+		System.out.println("Average decryption time, constant l = 100: " + averageDecryptionTime + " seconds");
+		
+		//return a double array -- could run some statistical analyses on the sampling distributions of mean times for a sample size of n = 100
+		double[] result = new double[4];
+		result[0] = averageSetupTime;
+		result[1] = averageEncryptionTime;
+		result[2] = averageKeyGenTime;
+		result[3] = averageDecryptionTime;
+		
+		return result;
+		
+}
+	
+	
 	
 	public static void main(String[] args) {
 		System.load("/Users/arushchhatrapati/Documents/mcl/lib/libmcljava.dylib");
-		Mcl.SystemInit(Mcl.BN254);
-		ArrayList<Integer> S = new ArrayList<Integer>();
-		int n = 100;
-		int l = 20; //max subset size
+		testRuntimes(Mcl.BLS12_381);
+		/*ArrayList<Integer> S = new ArrayList<Integer>();
+		int n = 100000;  //i = 1 to i = 100000
+		int l = 30; //max subset size
 		int i = 53;
-		S.addAll(Arrays.asList(5, 4, 8, 6, 9, 12, 15, 16, 45, 53));
+		S.addAll(Arrays.asList(4, 5, 8, 6, 9, 12, 15, 16, 45, 53));
 		Object[] setup = setup(n, l);
 		ArrayList<Object> PK = (ArrayList<Object>) setup[0];
 		Fr[] MSK = (Fr[]) setup[1];
@@ -363,7 +470,9 @@ public class IBBESystem {
 		GT K1 = (GT) C[2];
 		GT K2 = decrypt(S, i, di, tau, Hdr, PK);
 		System.out.println("K1 = " + K1);
-		System.out.println("K2 = " + K2); 
+		System.out.println("K2 = " + K2);*/
 	}
+	
+	
 	
 }
