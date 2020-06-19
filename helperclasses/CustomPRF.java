8f650d8ee1d93ff39af1a11108a7bbb6051804bd
ArushC <65872167+ArushC@users.noreply.github.com>
2020-06-19 12:34:20 -0700

    Update CustomPRF.java

--- helperclasses/CustomPRF.java
+++ helperclasses/CustomPRF.java
@@ -28,14 +28,20 @@ public class CustomPRF {
 	}
 	
 	
-	public Fr compute() {
+	public Fr compute(int lambda) {
+		//The Fr NEEDS to be computed over BN254 otherwise it doesn't work
+		if (lambda == Mcl.BLS12_381)
+			Mcl.SystemInit(Mcl.BN254); //switch to BN254
 		G1 g1 = new G1();
 		Mcl.hashAndMapToG1(g1, key.getBytes()); 
 		String g1String = g1.toString();
 		int endIndex = g1String.indexOf(' ', 2); 
 		Fr result = new Fr(g1String.substring(2, endIndex)); //result is defined as the first Fr that appears in the G1 string
+		if (lambda == Mcl.BLS12_381) //switch back to BLS12_381
+			Mcl.SystemInit(Mcl.BLS12_381);
 		return result;
 	}
 	
 	
 }
+
