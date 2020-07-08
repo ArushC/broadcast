42aa554fa37b97506c3da670f5b6556b827fb23d
ArushC <65872167+ArushC@users.noreply.github.com>
2020-07-07 18:00:35 -0700

    Create Node.java

---
+++ helperclasses/Node.java
@@ -0,0 +1,22 @@
+package helperclasses;
+
+public class Node{
+	private int datum;
+	
+	public Node(int datum){
+		this.datum = datum;
+	}
+	
+	public boolean equals(Node other) {
+		return other.datum == this.datum;
+	}
+	
+	public int getDatum() {
+		return this.datum;
+	}
+	
+	@Override
+	public String toString() {
+		return Integer.toString(datum);
+	}
+}
