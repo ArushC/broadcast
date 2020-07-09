b222d8d1cd9102ec6ef59e7a5801fca66014df8f
ArushC <65872167+ArushC@users.noreply.github.com>
2020-07-08 17:01:52 -0700

    Update Baseline.java

--- schemes/Baseline.java
+++ schemes/Baseline.java
@@ -1,6 +1,9 @@
 package schemes;
+import helperclasses.Tools;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
 
 import com.herumi.mcl.*;
 //El Gamal Encryption With Shared Parameters
@@ -83,9 +86,105 @@ public class BaselineScheme {
 		return res;	
 	}
 	
+	//returns long[] containing elapsed time for setup, encrypt, and decrypt, respectively
+		private static long[] printRuntimes(int lambda, int N, int subsetSize) {
+			
+			//Get elapsed time for setup(n) 
+			long startSetup = System.nanoTime();
+			Object[] setup = setup(lambda, N);
+			long elapsedSetup = System.nanoTime() - startSetup;
+			double secondsSetup = ((double) elapsedSetup) / 1E9;
+			//extract public/private key from setup
+			ArrayList<G1> PK = (ArrayList<G1>) setup[0];
+			ArrayList<Fr> privateKeys = (ArrayList<Fr>) setup[1];
+			
+			
+			ArrayList<Integer> S = new ArrayList<Integer>();
+			//randomly generate numbers to put in the subset S (NO REPEATS)
+			ArrayList<Integer> randomNums = new ArrayList<Integer>();
+			for (int i = 0; i < N; i++) { 
+				randomNums.add(i + 1);
+			}
+			for (int i = 0; i < subsetSize; i++) {
+				int randomIndex = ThreadLocalRandom.current().nextInt(1, randomNums.size());
+				int randomID = randomNums.get(randomIndex);
+				S.add(randomID);
+				randomNums.remove(randomIndex);
+			}
+			
+			//Generate random message M
+			G1 M = new G1();
+			Mcl.hashAndMapToG1(M, Tools.generateRandomBytes(3));
+			
+			//Get elapsed time for encrypt
+			long startEncrypt = System.nanoTime();
+			G1[] CT = enc(PK, S, M);
+			long elapsedEncrypt = System.nanoTime() - startEncrypt;
+			double secondsEncrypt = ((double) elapsedEncrypt) / 1E9;
+			//Get random user ID i to test the decryption
+			int i = S.get(ThreadLocalRandom.current().nextInt(0, S.size()));
+			Fr xi = privateKeys.get(i - 1);
+			
+			//Get elapsed time for decrypt
+			long startDecrypt = System.nanoTime();
+			G1 M1 = dec(CT, xi, i);
+			long elapsedDecrypt = System.nanoTime() - startDecrypt;
+			double secondsDecrypt = ((double) elapsedDecrypt) / 1E9;
+			//Finally, print out the results
+			
+			String success = (M.equals(M1)) ? "SUCCESSFUL DECRYPTION" : "FAILED DECRYPTION";
+			System.out.println(success + ": " + "n = " + N + ", subset size = " + subsetSize);
+			System.out.println(); //padding
+			System.out.println("setup took " + secondsSetup + " seconds");
+			System.out.println("encryption took " + secondsEncrypt + " seconds");
+			System.out.println("decryption took " + secondsDecrypt + " seconds (i = " + i + ")");
+			System.out.println(); //more padding
+			
+			long[] elapsedTimes = new long[3];
+			elapsedTimes[0] = elapsedSetup;
+			elapsedTimes[1] = elapsedEncrypt;
+			elapsedTimes[2] = elapsedDecrypt;
+			
+			return elapsedTimes;
+		}
+		
+		//Test the runtimes of the algorithms
+		public static void testRuntimes(int lambda) {
+			
+			//see how runtime changes with constant n and increasing subset size
+			long totalSetupTime = 0;
+			
+			for (int i = 100; i <= 2000; i+=100) {
+				long[] elapsedTimes = printRuntimes(lambda, 10000, i);
+				totalSetupTime += elapsedTimes[0];
+			}
+			
+			double averageSetupTime = ((double) totalSetupTime) / (1E9 * 20);
+			
+			
+			long totalEncryptionTime = 0;
+			long totalDecryptionTime = 0;
+			//see how runtime changes with increasing n, constant subset size = 100
+			for (int i = 1000; i <= 20000; i += 1000) {
+				long[] elapsedTimes =  printRuntimes(lambda, i, 100);
+				totalEncryptionTime += elapsedTimes[1];
+				totalDecryptionTime += elapsedTimes[2];
+			}
+			
+			double averageEncryptionTime = ((double) totalEncryptionTime) / (1E9 * 20);
+			double averageDecryptionTime = ((double) totalDecryptionTime) / (1E9 * 20);
+			
+			System.out.println("Average setup time, constant n = 10000: " + averageSetupTime + " seconds");
+			System.out.println("Average encryption time, constant subset size = 100: " + averageEncryptionTime + " seconds");
+			System.out.println("Average decryption time, constant subset size = 100: " + averageDecryptionTime + " seconds");
+		
+		}
+	
 	public static void main(String[] args) {
 		System.load("/Users/arushchhatrapati/Documents/mcl/lib/libmcljava.dylib");
-		Object[] setup = setup(Mcl.BN254, 100);
+		testRuntimes(Mcl.BLS12_381);
+		//printRuntimes(Mcl.BN254, 1000000, 100);
+		/*Object[] setup = setup(Mcl.BN254, 100);
 		ArrayList<G1> PK = (ArrayList<G1>) setup[0];
 		ArrayList<Fr> SK = (ArrayList<Fr>) setup[1];
 		ArrayList<Integer> S = new ArrayList<Integer>();
@@ -97,6 +196,10 @@ public class BaselineScheme {
 		G1[] CT = enc(PK, S, M);
 		Fr xi = SK.get(i - 1);
 		G1 M1 = dec(CT, xi, i);
-		System.out.println("M1 = " + M1);
-	}	
+		System.out.println("M1 = " + M1);*/
+	}
+	
+	
+	
 }
+
