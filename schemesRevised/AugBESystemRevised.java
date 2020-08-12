db87cad4cd28d7a8096552b21141bde8779447ae
ArushC <65872167+ArushC@users.noreply.github.com>
2020-08-12 08:02:48 -0700

    Update AugBESystemRevised.java

--- schemesRevised/AugBESystemRevised.java
+++ schemesRevised/AugBESystemRevised.java
@@ -537,7 +537,7 @@ public class AugBERevised {
 		
 	//Test the runtimes of the algorithms
 	public static void testRuntimes(int lambda, int percent) {
-		for (int N = 10; N <= 1000000; N *= 10) {
+		for (int N = 100; N <= 1000000; N *= 10) {
 			int subsetSize = (int) (0.01 * percent * N);
 			printRuntimes(N, subsetSize, lambda);
 		}	
