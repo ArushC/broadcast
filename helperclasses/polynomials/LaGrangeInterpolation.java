38fadff1aca60095639e5b168b8d43edb9afbd73
Arush Chhatrapati <65872167+ArushC@users.noreply.github.com>
2022-05-16 15:06:06 -0700

    Update LaGrangeInterpolation.java

--- helperclasses/polynomials/LaGrangeInterpolation.java
+++ helperclasses/polynomials/LaGrangeInterpolation.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 import java.math.MathContext;
 import java.math.RoundingMode;
 
-public class LagrangeInterpolation {
+public class LaGrangeInterpolation {
 	
 	//multiplies two polynomials
 	//input: coefficients of 1st polynomial, coefficients of second polynomial
